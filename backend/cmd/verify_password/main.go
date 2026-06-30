a94845e8b0ea51442921022730258e8406d5bc7d
udaBasrianto <basrianto@gmail.com>
2026-06-30 12:32:31 +0700

    refactor: move backend utility scripts to separate cmd directories to fix multiple main packages

---
+++ backend/cmd/verify_password/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	hash := "$2a$14$GJNqzKFN6Xyy5moh0y83pO9qa0Bm9Ikr7vkrTLGVAMrfKRQon6d8S"
+	password := "mas@abd.com"
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	fmt.Println("compare err:", err)
+}
